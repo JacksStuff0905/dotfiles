3f17ce7adde962a40bf8ae753211305f69bbd553
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2025-07-22 00:11:28 +0200

    Added lualine to lazy

--- nvim/.config/nvim/lua/conf/keymap.lua
+++ nvim/.config/nvim/lua/conf/keymap.lua
@@ -1,5 +1,7 @@
 -- Configure telescope
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>fd', builtin.find_files, {})
-vim.keymap.set('n', '<leader>grep', builtin.live_grep, {})
+vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
 
+-- Configure neotree
+vim.keymap.set('n', '<C-t>', ':Neotree filesystem toggle left<CR>', {})

7c609a45c9843bdf3ebc825498aa090ea8cde1f6
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2025-07-22 14:22:57 +0200

    Added lsp config to nvim

--- nvim/.config/nvim/lua/conf/keymap.lua
+++ nvim/.config/nvim/lua/conf/keymap.lua
@@ -5,3 +5,8 @@ vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
 
 -- Configure neotree
 vim.keymap.set('n', '<C-t>', ':Neotree filesystem toggle left<CR>', {})
+
+-- Configure lsp
+vim.keymap.set('n', 'H', vim.lsp.buf.hover, {})
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})

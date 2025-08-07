69d773afc412d76b428542a8ccfbc164bf60ca2d
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2025-08-07 11:52:15 +0200

    Added image.nvim plugin for neotree

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -7,7 +7,7 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- Optional image support for file preview: See `# Preview Mode` for more information.
-      -- {"3rd/image.nvim", opts = {}},
+      {"3rd/image.nvim", opts = {}},
       -- OR use snacks.nvim's image module:
       -- "folke/snacks.nvim",
     },

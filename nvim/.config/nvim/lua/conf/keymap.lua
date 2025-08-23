af47f99f220a524bdfb01b42fb19407d5fdb1a31
Jacek Sawiński <jacek.sawinski.0905@gmail.com>
2025-08-23 21:13:23 +0200

    Updated nvim config

--- nvim/.config/nvim/lua/conf/keymap.lua
+++ nvim/.config/nvim/lua/conf/keymap.lua
@@ -3,44 +3,53 @@ vim.cmd("ca w!! SudaWrite")
 
 -- Configure telescope
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>fd', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fd", builtin.find_files, {})
+vim.keymap.set("n", "<leader>gr", builtin.live_grep, {})
 
 -- Configure neotree
-vim.keymap.set('n', '<leader>fs', ':Neotree filesystem toggle left<CR>', {})
+vim.keymap.set("n", "<leader>fs", ":Neotree filesystem toggle left<CR>", {})
 
 -- Configure lsp
-vim.keymap.set('n', '<leader>dc', vim.lsp.buf.hover, {})
-vim.keymap.set('n', '<leader>df', vim.lsp.buf.definition, {})
-vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "<leader>dc", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "<leader>df", vim.lsp.buf.definition, {})
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
 -- Configure formatters
-vim.keymap.set('n', '<leader>fr', vim.lsp.buf.format, {})
+vim.keymap.set("n", "<leader>fr", vim.lsp.buf.format, {})
 
 -- Configure linters
-vim.keymap.set('n', '<leader>er', vim.diagnostic.open_float, {})
+vim.keymap.set("n", "<leader>er", vim.diagnostic.open_float, {})
 
 -- Configure colorizer
 function toggle_colorizer(c)
-  if c.is_buffer_attached(0) then c.detach_from_buffer(0) else c.attach_to_buffer(0) end
+  if c.is_buffer_attached(0) then
+    c.detach_from_buffer(0)
+  else
+    c.attach_to_buffer(0)
+  end
 end
 
-local colorizer = require('colorizer')
-vim.keymap.set('n', '<leader>cl', function() toggle_colorizer(colorizer) end, {})
-
+local colorizer = require("colorizer")
+vim.keymap.set("n", "<leader>cl", function()
+  toggle_colorizer(colorizer)
+end, {})
 
 -- Configure debuggers
 local dap = require("dap")
-vim.keymap.set('n', '<leader>br', dap.toggle_breakpoint, {})
-vim.keymap.set('n', '<leader>cn', dap.continue, {})
-
+vim.keymap.set("n", "<leader>br", dap.toggle_breakpoint, {})
+vim.keymap.set("n", "<leader>cn", dap.continue, {})
 
 -- Configure window navigation
-vim.keymap.set('n', 'H', '<C-w>h', { desc = 'Move to left window' })
-vim.keymap.set('n', 'J', '<C-w>j', { desc = 'Move to lower window' })
-vim.keymap.set('n', 'K', '<C-w>k', { desc = 'Move to upper window' })
-vim.keymap.set('n', 'L', '<C-w>l', { desc = 'Move to right window' })
+vim.keymap.set("n", "H", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "J", "<C-w>j", { desc = "Move to lower window" })
+vim.keymap.set("n", "K", "<C-w>k", { desc = "Move to upper window" })
+vim.keymap.set("n", "L", "<C-w>l", { desc = "Move to right window" })
 
+vim.keymap.set("n", "<leader>bc", "<C-o>", { desc = "Return to previous window" })
+vim.keymap.set("n", "<leader>fw", "<C-i>", { desc = "Return to next window" })
 
-vim.keymap.set('n', '<leader>bc', '<C-o>', { desc = 'Return to previous window' })
-vim.keymap.set('n', '<leader>fw', '<C-i>', { desc = 'Return to next window' })
+-- Configure highlighting
+vim.keymap.set("n", "<leader>nh", ":noh<CR>", { desc = "Hide highlight" })
+
+-- Configure renaming
+vim.keymap.set("n", "<leader>rn", require("renamer").rename, { desc = "Rename" })
